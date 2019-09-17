d65995b838799a7f76276c15964d387117a65870
Kyle Bustard <bustardkyle@gmail.com>
2019-09-16 21:49:46 -0500

    Add GetUsers and AddUser functions to User model

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kylebustard/lowendcode/models"
+)
 
 func main() {
-	fmt.Println("Hello from lowendcode, Gophers!")
+	u := models.User{
+		ID:        2,
+		FirstName: "Selim",
+		LastName:  "Wormrider",
+	}
+	fmt.Println(u)
+}
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
 }
